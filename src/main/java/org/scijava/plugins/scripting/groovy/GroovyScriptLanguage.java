c548ec0731a17183aced940ab8f4037d34ef0eff
Curtis Rueden <ctrueden@wisc.edu>
2021-10-10 16:26:56 -0500

    Remember imports between eval calls

--- src/main/java/org/scijava/plugins/scripting/groovy/GroovyScriptLanguage.java
+++ src/main/java/org/scijava/plugins/scripting/groovy/GroovyScriptLanguage.java
@@ -29,13 +29,24 @@
 
 package org.scijava.plugins.scripting.groovy;
 
-import javax.script.ScriptEngine;
+import java.security.CodeSource;
 
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import org.codehaus.groovy.ast.CompileUnit;
+import org.codehaus.groovy.ast.ModuleNode;
+import org.codehaus.groovy.control.CompilationUnit;
+import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.jsr223.GroovyScriptEngineFactory;
+import org.codehaus.groovy.jsr223.GroovyScriptEngineImpl;
 import org.scijava.Priority;
 import org.scijava.plugin.Plugin;
 import org.scijava.script.AdaptedScriptLanguage;
 import org.scijava.script.ScriptLanguage;
+
+import groovy.lang.GroovyClassLoader;
 
 /**
  * An adapter of the Groovy interpreter to the SciJava scripting interface.
@@ -46,7 +57,64 @@ import org.scijava.script.ScriptLanguage;
 @Plugin(type = ScriptLanguage.class, name = "Groovy", priority = Priority.HIGH)
 public class GroovyScriptLanguage extends AdaptedScriptLanguage {
 
+	private static final ThreadLocal<CompileUnit> asts = new ThreadLocal<>();
+
 	public GroovyScriptLanguage() {
 		super(new GroovyScriptEngineFactory());
+	}
+
+	@Override
+	public ScriptEngine getScriptEngine() {
+		// Attach a "compiler customizer" (a Groovy feature) to the script engine,
+		// which will feed extra imports into each script prior to evaluation.
+		final CompilerConfiguration compilerConfig = new CompilerConfiguration();
+		final ImportRetainer importRetainer = new ImportRetainer();
+		compilerConfig.addCompilationCustomizers(importRetainer);
+
+		final GroovyScriptEngineImpl engine = new GroovyScriptEngineImpl() {
+
+			@Override
+			public Object eval(String script, ScriptContext scriptContext) throws ScriptException {
+				final Object result = super.eval(script, scriptContext);
+
+				// Retrieve the AST that was built during compilation+evaluation.
+				final CompileUnit ast = asts.get();
+				asts.remove();
+
+				// Extract the imports present in the AST, and save them for next time.
+				for (final ModuleNode m : ast.getModules()) {
+					m.getImports().forEach(ipt -> importRetainer.retainImport(ipt));
+					m.getStarImports().forEach(ipt -> importRetainer.retainStarImport(ipt));
+					m.getStaticImports().values().forEach(ipt -> importRetainer.retainStaticImport(ipt));
+					m.getStaticStarImports().values().forEach(ipt -> importRetainer.retainStaticStarImport(ipt));
+				}
+				return result;
+			}
+		};
+
+		// Wrap the script engine's ClassLoader in another layer, which saves AST
+		// objects as soon as they are created. In this way, we can retrieve the AST
+		// of each particular evaluation after it completes. This is important so
+		// that we can extract the imports of that evaluation, to reapply them when
+		// evaluating subsequent scripts.
+		final GroovyClassLoader classLoader = new GroovyClassLoader(engine.getClassLoader(), compilerConfig, false) {
+
+			@Override
+			protected CompilationUnit createCompilationUnit(CompilerConfiguration config, CodeSource source) {
+				// Create the compilation unit as usual.
+				final CompilationUnit unit = super.createCompilationUnit(config, source);
+
+				// Obtain the AST that now exists as part of the unit creation.
+				final CompileUnit ast = unit.getAST();
+
+				// Save the AST into a ThreadLocal, for post-eval retrieval.
+				asts.set(ast);
+
+				return unit;
+			}
+		};
+		engine.setClassLoader(classLoader);
+
+		return engine;
 	}
 }
